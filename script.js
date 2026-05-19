ee44d89d4eccdcf9fe3ab1954bca3ecc43383548
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2026-05-19 11:31:22 -0400

    gameState logic

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+let gameState = 'Rules';
+
 document.addEventListener('keydown', function(e) {
     if (e.code === 'Space') {
-        document.getElementById('Rules').style.display = 'none';
-        document.getElementById('Game').style.display = 'block';
+        if (gameState === 'Rules') {
+            gameState = 'Game';
+            document.getElementById('Rules').style.display = 'none';
+            document.getElementById('Game').style.display = 'block';
+        } else if (gameState === 'Game') {
+            gameState = 'Game-Over';
+            document.getElementById('Game').style.display = 'none';
+            document.getElementById('Game-Over').style.display = 'block';
+        } else if (gameState === 'Game-Over') {
+            gameState = 'Game';
+            document.getElementById('Game-Over').style.display = 'none';
+            document.getElementById('Game').style.display = 'block';
+        }
     }
 })
