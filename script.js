8c34ba792662af9c2745a3542b2652ca9f5cf624
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2026-05-20 10:26:48 -0400

    Minor progress

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ let gameState = 'Rules';
 
 document.addEventListener('keydown', function(e) {
     if (e.code === 'Space') {
+        console.log(gameState) // debug
         if (gameState === 'Rules') {
-            gameState = 'Game';
+            gameState = 'Waiting';
             document.getElementById('Rules').style.display = 'none';
-            document.getElementById('Game').style.display = 'block';
-        } else if (gameState === 'Game') {
+            document.getElementById('Game').style.display = 'flex';
+        } else if (gameState === 'Waiting') {
+            // wait for box to change color
+        } else if (gameState === 'Reacting') {
             gameState = 'Game-Over';
             document.getElementById('Game').style.display = 'none';
             document.getElementById('Game-Over').style.display = 'block';
         } else if (gameState === 'Game-Over') {
-            gameState = 'Game';
+            gameState = 'Waiting';
             document.getElementById('Game-Over').style.display = 'none';
-            document.getElementById('Game').style.display = 'block';
+            document.getElementById('Game').style.display = 'flex';
         }
     }
 })
