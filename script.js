e57e847ad5a50fc41d611a36ec8020a33cf35f8a
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2026-05-22 13:05:02 -0400

    Reaction speed using performance.now()

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let gameState = 'Rules';
+let colorChangeTime = 0;
+
 
 function startTimer() {
-    let randomDelay = Math.random() * (5000 - 5000) + 5000;
+    let randomDelay = Math.random() * (7500 - 5000) + 5000;
     setTimeout(function() {
         document.getElementById('Box').style.backgroundColor = '#f1e05a' 
+        colorChangeTime = performance.now(); // right when box changes color
         gameState = 'Reacting';
     }, randomDelay);
 }
@@ -18,6 +21,8 @@ document.addEventListener('keydown', function(e) {
             startTimer();
         } else if (gameState === 'Waiting') {
         } else if (gameState === 'Reacting') {
+            let reactionTime = performance.now() - colorChangeTime;
+            console.log(reactionTime);
             gameState = 'Game-Over';
             document.getElementById('Game').style.display = 'none';
             document.getElementById('Game-Over').style.display = 'block';
